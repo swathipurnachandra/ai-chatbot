d9e3434c850f95ccf22da7bee464d3359c71dd7a
swathipurnachandra <swathipurnachandra28@gmail.com>
2025-07-08 15:25:34 +0530

    Initial commit made

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+function App() { 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to the AI Chatbot</h1>
+        <p>This is a simple chatbot application.</p>
       </header>
     </div>
   );
